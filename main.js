083039a72e633e872484795b9c062382775f70f0
reehmahcodes <abdulazeezroheemoh@gmail.com>
2023-11-30 02:59:27 +0100

    Login Page

---
+++ main.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector ('.login-link');
+const registerlink = document.querySelector ('.register-link');
+
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+const hamdis = document.querySelector('.ham-dis');
+
+registerlink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginlink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+hamdis.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+ 
